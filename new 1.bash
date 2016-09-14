f7418d2198d536a41a6f9a4e694efde412a5127e
artur192 <atammist@itcollege.ee>
2016-09-14 15:39:31 +0300

    Veeb gitti

---
+++ new 1.bash
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    
+<meta charset="utf-8"/>
+  </head>
+  <body>
+      <p>Tere mina olen Artur Mäetaguselt</p>
+      <img src="http://www.facebook.com/artur.tammiste"/>
+  </body>
+</html>
